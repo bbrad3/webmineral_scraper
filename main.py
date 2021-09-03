6c97906cbdf1900c7848f6c82bf1fcfe4cc45f73
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-09-03 15:46:50 -0600

    Changed csv formating on write, etc.

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import csv
+import time
+
+# Time run
+start_time = int(round(time.time(), 2))
 
 source = requests.get('http://www.webmineral.com/data/index.html').text
 
 soup = BeautifulSoup(source, 'lxml')
 
 # open the file you want to write to
-csv_file = open('portfolio_scraper', 'w')
+# change the filename to create a new one
+csv_file = open('webmineral_scrape.csv', 'w')
 
 # initialize writer
 csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['webmineral.com'])
 
 # find a tags
 a_tags = soup.find_all('a')
@@ -25,18 +31,23 @@ mineral_names.remove('/')
 # --HELPER FUNCTIONS--
 
 
+# format and write chem formula to csv
 def get_chem_formula(string: str):
     split_text = string.split(':')
-    # WRITE CHEM_FORMULA TO CSV
     csv_writer.writerow([split_text[0], split_text[1]])
     # print(split_text)
+
+# format and write molecular weight to csv
 
 
 def get_mol_wt(string: str):
     s = string.split(':')
     mol_wt = s[1].strip().split('=')
-    csv_writer.writerow([mol_wt[0], mol_wt[1]])
+    val = mol_wt[1].split(' ')
+    csv_writer.writerow([mol_wt[0], val[0], val[2]])
     # print(mol_wt)
+
+# format and write composition to csv
 
 
 def get_composition(arr: list):
@@ -49,31 +60,53 @@ def get_composition(arr: list):
             sym = split_s[4].replace('%', '')
             if elem == '':
                 elem = 'Total'
-            csv_writer.writerow([elem, sym, '%', val])
-            print(f'{elem}({sym}, %) {val}')
+            csv_writer.writerow([elem, sym, val, '%'])
+            # print(f'{elem}({sym}, %) {val}')
 
 
+# this will be the total number of pages scraped
+success = 0
+# total Exceptions thrown
+fail = 0
 for mineral in mineral_names:
     try:
-        # scrape each link
+        # soup-ify each link
         link = 'http://www.webmineral.com/data/{0}#.YTEkIdPnNqs'.format(
             mineral)
         mineral_source = requests.get(link).text
         mineral_soup = BeautifulSoup(mineral_source, 'lxml')
+        # get the html elements we want
         td_25 = mineral_soup.find_all("td", width="25%", limit=10)
+        # trim the text to an acceptable format
         all_text = []
         for td in td_25:
             found_text = td.parent.text
             trimmed_text = found_text.strip().replace('\n', '').replace('\xa0', '')
             all_text.append(trimmed_text)
 
-        # print(all_text)
-        csv_writer.writerow([mineral[:-6]])
-        csv_writer.writerow([])
+        mineral_name = str(mineral[:-6]).replace('%20', ' ')
+        print(mineral_name)
+        # use helper functions to extract needed data
+        csv_writer.writerow([mineral_name])
         get_chem_formula(all_text[0])
         get_mol_wt(all_text[1])
-        csv_writer.writerow([])
         get_composition(all_text[0:10])
-        break
+        # get_img
+        csv_writer.writerow([])
+        success += 1
+        # uncomment to restrict loop
+        # if success >= 5:
+        #     break
+
     except Exception as err:
+        fail += 1
         print(err)
+
+end_time = int(round(time.time(), 2))
+# total_time is in min
+total_time = (end_time - start_time) / 60
+print(f'DONE! Total Time: {total_time} min')
+print(f'Total success: {success}')
+print(f'Total fail: {fail}')
+# close file
+csv_file.close()
